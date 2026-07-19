4292e7d32a9daa84a83cc1c58d30fd00e82ea8c8
Muhammad Anas <mannas.632006@icloud.com>
2026-07-19 22:45:31 +0500

    Change round_to default from 2 to 4 decimal places

--- calculator.py
+++ calculator.py
@@ -21,6 +21,6 @@ def divide(a, b, safe=False):
     return a / b
 
 
-def round_to(value, places=2):
+def round_to(value, places=4):
     """Round value to the given number of decimal places."""
     return round(value, places)
